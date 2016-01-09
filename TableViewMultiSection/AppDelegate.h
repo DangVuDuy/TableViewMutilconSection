c66f765e92d020e3a35dc6f9e65df1242a0b48b0
Dang Vu Duy <Dev@dangs-macbook-air.local>
2016-01-09 14:55:34 +0700

    Initial Commit

---
+++ TableViewMultiSection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewMultiSection
+//
+//  Created by Dang Vu Duy on 1/9/16.
+//  Copyright © 2016 Dang Vu Duy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewMultiSection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewMultiSection
+//
+//  Created by Dang Vu Duy on 1/9/16.
+//  Copyright © 2016 Dang Vu Duy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
